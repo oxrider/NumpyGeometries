1ab223070d9be0c9e1d09aaf04394faf31756245
Tengyu Liu <24.jason.5@gmail.com>
2018-11-06 06:24:15 -0800

    added triangle area, angle between vectors

--- geometry.py
+++ geometry.py
@@ -20,3 +20,16 @@ def point2segment(p, a, b):
     # p is within AB
     return np.linalg.norm(p - proj)
 
+def triangle_areas(tris):
+    # Computes areas of triangles
+    # Args: tris N x 3 x 3
+    tris = tris.reshape((-1,3,3))
+    sides = np.linalg.norm(tris - tris[:,[1,2,0],:], axis=-1)
+    s = sides.sum(axis=-1)/2
+    return np.sqrt(s*(s-sides[:,0])*(s-sides[:,1])*(s-sides[:,2]))
+
+def angle_between_vectors(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'    """
+    cos = np.dot(v1, v2)
+    sin = np.linalg.norm(np.cross(v1, v2))
+    return np.arctan2(sin, cos)
